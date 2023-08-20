624168bb4030da8687a765dfc3ea61c772ecd762
Gaurav237 <gktiwary237@gmail.com>
2023-08-20 23:07:34 +0530

    initialized routes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,19 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 
+// routes
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+// created api endpoints to connect from frontend
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E');
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import * as openai from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+
+export default router;

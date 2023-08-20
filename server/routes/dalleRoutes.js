6abbe6ecae168f121ef8983db15106371c9abb03
Gaurav237 <gktiwary237@gmail.com>
2023-08-21 00:35:30 +0530

    dalleRoutes done

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,10 +1,42 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import * as openai from 'openai';
+// import * as openai from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const newOpenAI = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.send('Hello from DALL-E');
+});
+
+router.route('/').post(async (req, res) => {
+    try{
+        const { prompt } = req.body;
+
+        const aiResponse = await OpenAIApi.createImage({
+            prompt: prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+
+        return res.status(200).json({ 
+            photo: image 
+        });
+
+    }catch(err){
+        return res.status(500).send(err?.response.data.error.message);
+    }
+})
 
 export default router;
